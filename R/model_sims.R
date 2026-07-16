cc9adbd539ba2a83cf7643c291b9dfc9d0922138
Marius 't Hart <thartbm@gmail.com>
2026-07-16 13:27:45 -0400

    exponential function distribution simulation runs, but still needs to be tested

--- R/model_sims.R
+++ R/model_sims.R
@@ -28,7 +28,7 @@ generateSimpleStepfunctionDistribution <- function(signal) {
     
     if (signal == 'adapt') {
       step_size_distr <- step_size_distributions[step_size_distributions$rotation==rotation,]
-      step_size_distr <- data.frame('m'=c(step_size_distr$mean, 0), 's'=c(step_size_distr$sd,1), 'w'=c(1,0))
+      step_size_distr <- data.frame('m'=c(0, step_size_distr$mean), 's'=c(1,step_size_distr$sd), 'w'=c(0,1))
     } else {
       step_size_distr <- step_size_distributions[step_size_distributions$rotation==rotation,c('m','s','w')]
     }
@@ -56,7 +56,7 @@ bootstrapStepWiseModel <- function(step_size_distr, step_time_distr, step_SD_dis
   results <- matrix(0, nrow = n_simulations, ncol = trials)
   
   # step or no step?
-  mode <- as.integer( runif(n_simulations) > step_size_distr$w[2] ) + 1
+  mode <- as.integer( runif(n_simulations) < step_size_distr$w[2] ) + 1
   
   # step sizes (0 for no step):
   step_sizes <- rep(0, n_simulations)
@@ -103,85 +103,153 @@ generateExponentialFunctionDistribution <- function(signal) {
     cat('signal must be either "aiming" or "adapt"\n')
   }
   
-  exp_asymptote_distr_name <- list('aiming' = 'multimodal', 'adapt' = 'normal')[[signal]]
+  asymptote_distr_name <- list('aiming' = 'multimodal', 'adapt' = 'normal')[[signal]]
   
-  exp_asymptote_distributions <- read.csv(sprintf('data/distributions/%s_step_size_%s_parameters.csv', signal, step_size_distr_name), stringsAsFactors = FALSE)
-  step_time_distributions <- read.csv(sprintf('data/distributions/%s_step_time_gamma_parameters.csv', signal), stringsAsFactors = FALSE)
-  step_SD_distributions   <- read.csv(sprintf('data/distributions/%s_step_SD_gamma_parameters.csv', signal), stringsAsFactors = FALSE)
-  step_SD_distributions   <- step_SD_distributions[is.na(step_SD_distributions$makestep),]
+  asymptote_distributions <- read.csv(sprintf('data/distributions/%s_exp_asymptote_%s_parameters.csv', signal, asymptote_distr_name), stringsAsFactors = FALSE)
+  changerate_distributions <- read.csv(sprintf('data/distributions/%s_exp_changerate_exponential_parameter.csv', signal), stringsAsFactors = FALSE)
+  exponential_SD_distributions   <- read.csv(sprintf('data/distributions/%s_exp_sd_gamma_parameters.csv', signal), stringsAsFactors = FALSE)
+  # step_SD_distributions   <- step_SD_distributions[is.na(step_SD_distributions$makestep),]
   
+  # print(asymptote_distributions)
+  # print(changerate_distributions)
+  # print(step_SD_distributions)
   
-  # in order to have 1000 runs even in the 7.5% participants without a strategy in the 60 degree rotation
-  # we need to run 15000 simulations in total (1000/0.075 = 13333.33... round up to be sure)
   n_simulations <- 20000
   simulations <- list()
   
   for (rotation in c(20,30,40,50,60)) {
     
     if (signal == 'adapt') {
-      step_size_distr <- step_size_distributions[step_size_distributions$rotation==rotation,]
-      step_size_distr <- data.frame('m'=c(step_size_distr$mean, 0), 's'=c(step_size_distr$sd,1), 'w'=c(1,0))
+      asymp_distr <- asymptote_distributions[asymptote_distributions$rotation==rotation,]
+      asymp_distr <- data.frame('m'=c(asymp_distr$mean, 0), 's'=c(asymp_distr$sd,1), 'w'=c(1,0))
     } else {
-      step_size_distr <- step_size_distributions[step_size_distributions$rotation==rotation,c('m','s','w')]
+      asymp_distr <- asymptote_distributions[asymptote_distributions$rotation==rotation,c('m','s','w')]
     }
-    step_time_distr <- step_time_distributions[step_time_distributions$rotation==rotation,c('shape','rate')]
-    step_SD_distr   <- step_SD_distributions[step_SD_distributions$rotation==rotation,c('shape','rate')]
     
-    AR <- bootstrapExponentialModel(step_size_distr = step_size_distr, 
-                                 step_time_distr = step_time_distr, 
-                                 step_SD_distr   = step_SD_distr, 
-                                 n_simulations   = n_simulations)
+    # print(asymp_distr)
+    changerate_distr <- changerate_distributions[changerate_distributions$rotation==rotation,]
+    # print(changerate_distr)
+    exp_SD_distr   <- exponential_SD_distributions[exponential_SD_distributions$rotation==rotation,c('shape','rate')]
+    # print(exp_SD_distr)
+
+    AR <- bootstrapExponentialModel( asymp_distr      = asymp_distr,
+                                     changerate_distr = changerate_distr,
+                                     exp_SD_distr     = exp_SD_distr,
+                                     n_simulations    = n_simulations)
+    print(str(AR))
     
-    simulations[[as.character(rotation)]] <- AR
-    
+    # simulations[[as.character(rotation)]] <- AR
+
   }
-  
-  saveRDS(simulations, file = sprintf('data/simulations/%s_stepfunction_simulations.rds', signal))
+
+  # saveRDS(simulations, file = sprintf('data/simulations/%s_stepfunction_simulations.rds', signal))
   
 }
 
-bootstrapExponentialModel <- function(step_size_distr, step_time_distr, step_SD_distr, n_simulations = 20000) {
+bootstrapExponentialModel <- function(asymp_distr, 
+                                      changerate_distr, 
+                                      exp_SD_distr, 
+                                      n_simulations = 20000) {
   
   trials <- 120
   
   # Create a matrix to store the results
   results <- matrix(0, nrow = n_simulations, ncol = trials)
   
-  # step or no step?
-  mode <- as.integer( runif(n_simulations) > step_size_distr$w[2] ) + 1
+  # ~0 asymptote (1) or above-0 asymptote (2)?
+  mode <- as.integer( runif(n_simulations) > asymp_distr$w[1] ) + 1
+  
+  # asymptotes:
+  asymptotes <- rep(0, n_simulations)
+  asymptotes[mode == 1] <- rnorm(sum(mode == 1), mean = asymp_distr$m[1], sd = asymp_distr$s[1])
+  asymptotes[mode == 2] <- rnorm(sum(mode == 2), mean = asymp_distr$m[2], sd = asymp_distr$s[2])
+  
   
   # step sizes (0 for no step):
-  step_sizes <- rep(0, n_simulations)
-  step_sizes[mode == 1] <- 0
-  step_sizes[mode == 2] <- rnorm(sum(mode == 2), mean = step_size_distr$m[2], sd = step_size_distr$s[2])
+  change_rates <- rexp(n = n_simulations, rate = changerate_distr$rate)
+  # cat(sprintf("percentage change rates > 1: %0.1f\n", 100*sum(change_rates > 1)/length(change_rates))) 
+  change_rates[which(change_rates > 1)] <- 1 # cap at 1, otherwise the exponential function will overshoot the asymptote
   
-  # step times (NA for no step - should not be used later on, which will throw errors):
-  step_times <- rep(NA, n_simulations)
-  step_times[mode == 2] <- ceiling(rgamma( n=sum(mode == 2), 
-                                           shape = step_time_distr$shape, 
-                                           rate = step_time_distr$rate ) )
   
-  step_times[which(step_times > trials)] <- trials
+  rellevels <- mapply(function(r,t) {r^t}, change_rates, matrix(rep(c(0:(trials-1)), each=length(change_rates)), ncol=trials,nrow=length(change_rates)))
+  curves <- matrix(rellevels, nrow=length(change_rates))
   
-  # simple model has just one level of noise throughout:
-  step_SD <- rgamma(n=n_simulations, shape=step_SD_distr$shape, rate=step_SD_distr$rate)
+  # multiply each row of curves with the asymptote of that simulated participant
+  curves <- curves * matrix(rep(asymptotes, each=trials), nrow=n_simulations, ncol=trials)
+  
+  
+  # figure out the noise:
+  exp_SD <- rgamma(n=n_simulations, shape=exp_SD_distr$shape, rate=exp_SD_distr$rate)
   
   noise <- matrix(rnorm(n=trials*n_simulations,
                         mean=0,
-                        sd=rep(step_SD,each=trials)), # same SD for all trials in a simulated participant 
+                        sd=rep(step_SD,each=trials)), # same SD for all trials in a simulated participant
                   nrow=n_simulations,
                   ncol=trials,
-                  byrow = TRUE) 
+                  byrow = TRUE)
   
-  # steps are added in a loop... can't think of a better way right now
-  for (idx in which(mode == 2)) {
-    # cat(sprintf('idx: %d, step_time: %d, step_size: %.2f\n', idx, step_times[idx], step_sizes[idx]))
-    results[idx,c(max(1,step_times[idx]):trials)] <- step_sizes[idx]
+  # add noise to the curves:
+  responses <- curves + noise
+  
+  return(responses)
+  
+}
+
+
+
+# plot and compare fits ----
+
+readData <- function(signal) {
+  
+  if (signal %in% c('aiming','adapt')) {
+  } else {
+    cat('signal must be either "aiming" or "adapt"\n')
   }
   
-  # noise is added in one go:
-  aiming_responses <- results + noise
+  behavior <- list()
   
-  return(aiming_responses)
+  for (rot in c(20,30,40,50,60)) {
+    
+    rotfiles <- list.files(path = "data/summaries", 
+                           pattern = sprintf("SUMMARY_aiming%d", rot))
+    
+    responses <- matrix(NA, nrow = length(rotfiles), ncol = 120)
+    
+    for (rotfile_no in c(1:length(rotfiles))) {
+      
+      rotfile <- rotfiles[[rotfile_no]]
+      
+      # ppno <- ppno + 1
+      
+      ppid <- substr(strsplit(rotfile, "_")[[1]][3], 1, 6)
+      
+      # cat(sprintf('working on participant %d (%s, %d° rotation)\n', ppno, ppid, rot))
+      
+      # read participant data
+      data <- read.csv(sprintf("data/summaries/%s", rotfile))
+      
+      if (signal == 'aiming') { 
+        
+        responses[rotfile_no,] <- data$aimdeviation_deg[which(data$rotation_deg == -1 * rot)]
+        
+      } else if (signal == 'adapt') {
+        
+        responses[rotfile_no,] <- data$reachdeviation_deg[which(data$rotation_deg == -1 * rot)]
+        
+      }
+      
+    }
+    
+    behavior[[as.character(rot)]] <- responses
+    
+  }
+  
+  return(behavior)
+
+}
+
+plotAdaptationDataFits <- function() {
+  
+  
   
 }
